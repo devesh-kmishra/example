c1c746b3634a034963129c52b2db677e8fc6da19
Devesh Krishna Mishra <mdevesh47@gmail.com>
2025-07-17 01:12:03 +0530

    changed text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <div className='w-[100vw] h-[100vh] flex justify-center items-center text-6xl font-bold m-auto'>
-        Aur batao, Renée?
+        Aur batao, Renu?
       </div>
     </>
   );
